0cd5652a284fd609704e7085a42f9add0d1b1a7e
CamiloCalderOn <calderoncamilo905@gmail.com>
2026-02-20 11:19:33 -0500

    feat: agrega imagen personal

--- app/src/main/java/me/camilo/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/camilo/tallergitgithubapp/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.camilo.tallergitgithubapp.ui.theme.TallerGitGithubAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +27,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TallerGitGithubAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            Targeta()
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Texgfft(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    TallerGitGithubAppTheme {
-        Greeting("Android")
+fun Targeta(){
+    Column(modifier = Modifier.fillMaxSize().padding(70.dp), horizontalAlignment = Alignment.CenterHorizontally ) {
+        val painter = painterResource(id = R.drawable.petro)
+
+        Image(
+            painter = painterResource(id = R.drawable.petro), contentDescription = "presidente",
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(120.dp)
+
+
+        )
+
+
+
     }
+
+
+
 }
