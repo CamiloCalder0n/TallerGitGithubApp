096cf85791139ca8f2230901746b3338d48c69ad
CamiloCalderOn <calderoncamilo905@gmail.com>
2026-02-20 11:59:06 -0500

    feat: agrega descripcion de intereses academicos y profesionales

--- app/src/main/java/me/camilo/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/camilo/tallergitgithubapp/MainActivity.kt
@@ -7,19 +7,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import me.camilo.tallergitgithubapp.ui.theme.TallerGitGithubAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +44,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun Targeta(){
-    Column(modifier = Modifier.fillMaxSize().padding(70.dp), horizontalAlignment = Alignment.CenterHorizontally ) {
+    Column(modifier = Modifier.fillMaxSize().padding(30.dp), horizontalAlignment = Alignment.CenterHorizontally ) {
         val painter = painterResource(id = R.drawable.petro)
 
         Image(
@@ -46,9 +52,76 @@ fun Targeta(){
             modifier = Modifier
                 .clip(CircleShape)
                 .size(120.dp)
-
-
         )
+        Text( modifier = Modifier.padding(top = 25.dp, bottom = 5.dp),
+            text = "Juan Perez",
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text( modifier = Modifier.padding(top = 0.dp, bottom = 20.dp),
+            text = "Desarrollador Movil Jr.",
+            fontSize = 20.sp,
+            color = Color.Blue
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = 1.dp,
+            color = Color.Gray
+        )
+
+        Text( modifier = Modifier.fillMaxWidth().padding(top = 2.dp),
+            text = "Edad",
+            fontSize = 20.sp,
+            color = Color.LightGray,
+            textAlign = TextAlign.Start
+        )
+
+        Text( modifier = Modifier.fillMaxWidth().padding(top = 3.dp),
+            text = "20 años",
+            fontSize = 19.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Start
+        )
+
+        Text( modifier = Modifier.fillMaxWidth().padding(top = 3.dp),
+            text = "Correo",
+            fontSize = 20.sp,
+            color = Color.LightGray,
+            textAlign = TextAlign.Start
+        )
+
+        Text( modifier = Modifier.fillMaxWidth().padding(top = 3.dp),
+            text = "calderoncamilo905@gmail.com",
+            fontSize = 19.sp,
+            color = Color.Blue,
+            textAlign = TextAlign.Start,
+        )
+
+        Text( modifier = Modifier.fillMaxWidth().padding(top = 3.dp),
+            text = "Ciudad",
+            fontSize = 20.sp,
+            color = Color.LightGray,
+            textAlign = TextAlign.Start
+        )
+
+        Text( modifier = Modifier.fillMaxWidth().padding(top = 3.dp),
+            text = "Bucaramanga, Colombia",
+            fontSize = 19.sp,
+            color = Color.Black,
+            textAlign = TextAlign.Start
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = 1.dp,
+            color = Color.Gray
+        )
+
+
+
+
 
 
 
